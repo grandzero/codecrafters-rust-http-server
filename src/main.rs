88b2e21147a20ab9ea1091c40242b2e2458b758d
Bayram Utku Uzunlar <38724739+grandzero@users.noreply.github.com>
2023-11-03 20:18:55 +0300

    read peer_addr and write_all

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
-use std::net::TcpListener;
-
+use std::net::{TcpListener, TcpStream};
+use std::io::{Read, Write};
+fn handle_client(mut stream : TcpStream) {
+    println!("Incoming request from: {}", stream.peer_addr().unwrap());
+    stream.write_all(b"HTTP/1.1 200 OK\r\n\r\n").unwrap();
+}
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
@@ -8,8 +12,8 @@ fn main() {
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
+            Ok(mut _stream) => {
+                handle_client(_stream);
             }
             Err(e) => {
                 println!("error: {}", e);
