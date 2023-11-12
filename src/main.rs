7fb6043dcafd41d82787e9ce288d7f6ebfac04ce
Bayram Utku Uzunlar <38724739+grandzero@users.noreply.github.com>
2023-11-12 16:51:39 +0300

    refactor

--- src/main.rs
+++ src/main.rs
@@ -6,32 +6,42 @@ fn handle_client(mut stream: TcpStream) {
     stream.read(&mut buffer).unwrap();
     let request_details_as_str = String::from_utf8_lossy(&buffer[..]);
     let request_lines = request_details_as_str.split("\r\n");
+    let query_param;
+    let response: String;
     match request_lines.clone().next() {
         Some(request_line) => {
             let request_original_details: Vec<&str> = request_line.split(" ").collect();
             println!("Request details:{:?}", request_original_details);
 
-            if request_original_details[1].contains("/echo/") {
-                let query_param = request_original_details[1]
+            response = if request_original_details[1].contains("/echo/") {
+                query_param = request_original_details[1]
                     .split("/echo/")
                     .collect::<Vec<&str>>()[1];
-                let response = format!(
+                format!(
                     "HTTP/1.1 200 OK\r\nContent-Type:text/plain\r\nContent-Length: {}\r\n\r\n{}",
                     query_param.len(),
                     query_param
-                );
-                stream.write_all(response.as_bytes()).unwrap();
+                )
+
+                // if request_original_details[1].starts_with("/echo/") {
+                //     let query_param = request_original_details[1]
+                //         .split("/echo/")
+                //         .collect::<Vec<&str>>()[1];
+                //     println!("Query param:{}", query_param);
+                //     //response = format!("HTTP/1.1 200 OK\r\nContent-Type:text/plain\r\nContent-Length: {}\r\n\r\n{}", query_param.len(), query_param).as_str();
+                // } else if request_original_details[1] == "/" {
+                //     response =
+                //         "HTTP/1.1 200 OK\r\nContent-Type:text/plain\r\nContent-Length: 0\r\n";
+                // } else {
+                //     response = "HTTP/1.1 404 NOT FOUND\r\nContent-Type:text/plain\r\n\r\nContent-Length: 9\r\n\r\nNot Found";
+                // }
             } else if request_original_details[1] == "/" {
-                stream
-                    .write_all(
-                        b"HTTP/1.1 200 OK\r\nContent-Type:text/plain\r\nContent-Length: 0\r\n",
-                    )
-                    .unwrap();
+                "HTTP/1.1 200 OK\r\nContent-Type:text/plain\r\nContent-Length: 0\r\n".to_string()
             } else {
-                stream
-                    .write_all(b"HTTP/1.1 404 NOT FOUND\r\nContent-Type:text/plain\r\n\r\nContent-Length: 9\r\n\r\nNot Found ")
-                    .unwrap();
-            }
+                "HTTP/1.1 404 NOT FOUND\r\nContent-Type:text/plain\r\n\r\nContent-Length: 9\r\n\r\nNot Found".to_string()
+            };
+            println!("Response:{}", response);
+            stream.write(response.as_bytes()).unwrap();
         }
         None => {
             println!("No request line found");
